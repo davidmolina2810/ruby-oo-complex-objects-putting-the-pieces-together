f4dbda5ac193fbf0b3b60e050dabc39408e8f353
davidmolina2810 <davidmolina2810@gmail.com>
2020-10-06 19:22:32 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
 
+  attr_accessor :page_count, :genre, :author
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
 
